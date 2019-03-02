3e08f0cfd4d4b6dde0cf2d705a424472d36f4d57
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-03-02 12:14:41 -0500

    html_css_js

--- script.js
+++ script.js
@@ -1,31 +1,60 @@
-const perIncome = prompt("How much do you bring home each pay period after taxes and deductions to the nearest even dollar?");
-const payPeriod = prompt("How many times a year do you get paid? Enter 52 for weekly pay, 26 for biweekly, and 12 for monthly.");
-const monthIncome = (perIncome * payPeriod)/12;
-const rentBudget = prompt("How much is your rent each month?");
-const autoBudget = prompt("How much is the total of your car payment, insurance, gas, and other maintenance per month?");
-const debtBudget = prompt("How much is the total of your student loans, credit card minimums, personal loans, and other debt payments per month?");
-const careBudget = prompt("How much do you spend on child, pet or other dependent care per month?");
-const utilBudget = prompt("How much are your utilities, including phone and internet, each month?");
-const groceryBudget = prompt("How much does it cost to keep you fed each month?");
-const essentialBudget = rentBudget + autoBudget + debtBudget + careBudget + utilBudget + groceryBudget;
-if (essentialBudget < monthIncome) {
-    alert("Your essential expenses are greater than your monthly income. At this pace, you'll either go into debt or be unable to afford your bills. Please visit our resources page for more information on how to adjust your budget or increase your income.")
-}
-else {
-    const outBudget = prompt("How much do you spend out at restaurants (including coffeeshops and fast food) and bars each month?");
-    const shoppingBudget = prompt("How much do you spend on shopping each month?");
-    const entBudget = prompt("How much do you spend on entertainment, including cable, concerts, subscriptions, and video games, each month?");
-    const discBudget = outBudget + shoppingBudget + entBudget;
-}
-const totalBudget = discBudget + essentialBudget;
-if (totalBudget < monthIncome) {
-    alert("Your total spending is above your monthly income. Please view our resources page for ideas on how to trim your discretionary spending.");
-}
-    else {
-        if (totalBudget > (monthIncome * .9)) {
-        alert("Your spending is high, but manageable. Please view our resources page for ideas on how to trim your discretionary spending so you can save more each month.")
+$("button").click(gather)
+
+function gather() {
+  income = document.getElementById("income").value;
+  income = income.replace(/\D/g,'');
+  income = parseInt(income);
+  rent = document.getElementById("rent").value;
+  rent = rent.replace(/\D/g,'');
+  rent = parseInt(rent);
+  utilities = document.getElementById("utilities").value;
+  utilities = utilities.replace(/\D/g,'');
+  utilities = parseInt(utilities);
+  groceries = document.getElementById("groceries").value;
+  groceries = groceries.replace(/\D/g,'');
+  groceries = parseInt(groceries);
+  insurance = document.getElementById("insurance").value;
+  insurance = insurance.replace(/\D/g,'');
+  insurance = parseInt(insurance);
+  auto = document.getElementById("auto").value;
+  auto = auto.replace(/\D/g, '');
+  auto = parseInt(auto);
+  care = document.getElementById("care").value;
+  care = care.replace(/\D/g, '');
+  care = parseInt(care);
+  essential = rent + groceries + utilities + insurance + auto + care;
+  food = document.getElementById("food").value;
+  food = food.replace(/\D/g, '');
+  food = parseInt(food);
+  shopping = document.getElementById("shopping").value;
+  shopping = shopping.replace(/\D/g, '');
+  shopping = parseInt(shopping);
+  entertainment = document.getElementById("entertainment").value;
+  entertainment = entertainment.replace(/\D/g, '');
+  entertainment = parseInt(entertainment);
+  discretionary = food + shopping + entertainment;
+  spending = essential + discretionary;
+  result = income - essential - discretionary;
+  savings = (income * 0.20);
+
+  $(".results-data").empty();
+  if (result === 0) {
+    $(".results-data").append('<p> Please enter your information using the form.</p>');
+  }
+  else if (essential > income) {
+    $(".results-data").append('<p>Your essential expenses exceed your monthly income. You should look at increasing your income or making changes to your lifestyle.</p>');
+  }
+
+  else if (spending > income) {
+    $(".results-data").append('<p>You have enough income to meet your basic spending, but your discretionary expenses put you over budget. You should consider reducing your spending on things like restaurants, shopping, and entertainment.');
+  }
+  else {
+    if (result > savings) {
+     $(".results-data").append(
+      '<p> After your expenses you have $' + result + ' left in your budget. You should be setting aside $' + savings +' of this as savings.</p>');
     }
     else {
-        alert("You are saving at least 10 percent of your income each month, that's great! Please visit our resources page for ideas on how to maximize your savings.");
+      $(".results-data").append('<p> After your expenses you have $' + result + ' left in your budget, but you should be saving $' + savings + ' each month. Try to reduce discretionary expenses.</p>');
     }
+  }
 }
